7428ae58ee6760c9e839c26eab934977ee1b3803
Bartlomiej Wiecek <email@gmail.com>
2024-10-23 09:24:17 +0200

    Final test changes

--- test.Tests/AlbumDataTests.cs
+++ test.Tests/AlbumDataTests.cs
@@ -5,13 +5,25 @@ namespace test.Tests
         [Fact]
         public void ReadAlbumsFromFile_GivenPathToExistingTextFile_ReturnsAlbumDataListWithCorrectData()
         {
+            //arrange
             string path = "Data.txt";
-            List<AlbumData> actual = AlbumData.ReadAlbumsFromFile(path);
             List<AlbumData> expected = new List<AlbumData>()
             {
                 new AlbumData() { Artist = "Artist1", Album = "\"Album1\"", SongsNumber = 2, Year = 2000, DownloadNumber = 21321 },
                 new AlbumData() { Artist = "Artist2", Album = "\"Album2\"", SongsNumber = 5, Year = 2005, DownloadNumber = 231272 },
             };
+            string fileContents = "";
+            foreach (AlbumData albumData in expected)
+            {
+                fileContents += albumData.Artist + "\n" + albumData.Album + "\n" + albumData.SongsNumber.ToString() + "\n" + albumData.Year.ToString() + "\n" + albumData.DownloadNumber + "\n\n";
+            }
+            File.WriteAllText(path, fileContents);
+            
+            //act
+            List<AlbumData> actual = AlbumData.ReadAlbumsFromFile(path);
+            File.Delete(path);
+
+            //assert
             Assert.Equal(expected.Count, actual.Count);
             Assert.Equivalent(expected, actual);
         }
